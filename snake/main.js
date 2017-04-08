008757431b4ed9db117ddf348ab79c55b39ef5f3
Kath-IBU <kathf@hotmail.com>
2017-04-08 20:15:17 +1000

    Rename start to initialPosition

--- snake/main.js
+++ snake/main.js
@@ -1,36 +1,35 @@
 function renderGrid(size) {
-    var table = $('<table></table>');
+  var table = $('<table></table>');
 
   for(i=0; i<size; i++){
-      var row = $('<tr></tr>');
+    var row = $('<tr></tr>');
     for(j=0; j< size; j++){
-        var col = $('<td class="cell" id ="' + i + '-' + j + '"></td>');
-        row.append(col);
-      };
-      table.append(row);
+      var col = $('<td class="cell" id ="' + i + '-' + j + '"></td>');
+      row.append(col);
     };
-    $('#grid').append(table);
-  }
+    table.append(row);
+  };
+  $('#grid').append(table);
 }
 
+
 var snake = {
-  start : function() {
-    this.headPosition(20,20);
+  initialPosition : function(gridSize) {
+    var middle = Math.floor( gridSize / 2 );
+    this.headPosition(middle,middle);
     this.direction = 'right'
   },
   headPosition : function(x,y) {
-    var idName = "#" + x + "-" + y; 
+    var idName = "#" + x + "-" + y;
     $(idName).addClass('snakeHead');
   },
   bodyPositions : [[20,20]],
   move : function() {
-    
   }
 }
 
 var game = {
   start : function() {
-    snake.start();
     setInterval(snake.move(), 500);
   }
 }
@@ -38,6 +37,7 @@ var game = {
 $(document).ready(function(){
   var gridSize = 20;
   renderGrid(gridSize);
+  snake.initialPosition(gridSize);
   game.start();
   $(document).keydown(function(event){
     snake.direction = event.key.substring(5, event.key.length).toLowerCase();
