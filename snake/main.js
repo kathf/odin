6db66a8bc90d846d290ecd16813127a95daab126
Kath-IBU <kathf@hotmail.com>
2017-04-09 17:33:32 +1000

    Revert to grid object. Create gameOver function.

--- snake/main.js
+++ snake/main.js
@@ -1,34 +1,36 @@
-function renderGrid(size) {
-  var table = $('<table></table>');
+var grid = {
+  size: 20,
+  render: function () {
+    var table = $('<table></table>');
 
-  for(i=0; i<size; i++){
-    var row = $('<tr></tr>');
-    for(j=0; j< size; j++){
-      var col = $('<td class="cell" id ="' + i + '-' + j + '"></td>');
-      row.append(col);
-    };
-    table.append(row);
-  };
-  $('#grid').append(table);
-}
-
+    for ( i=0; i<grid.size; i++ ) {
+      var row = $('<tr></tr>');
+      for( j=0; j< grid.size; j++ ){
+        var col = $('<td class="cell" id ="' + i + '-' + j + '"></td>');
+        row.append(col);
+      }
+      table.append(row);
+    }
+    $('#grid').append(table);
+  }
+};
 
 var snake = {
-  initialPosition : function(gridSize) {
-    var middle = Math.floor( gridSize / 2 );
+  initialPosition: function() {
+    var middle = Math.floor( grid.size / 2 );
     snake.headPosition = [middle,middle];
     snake.direction = 'right';
+    snake.render();
   },
-  render : function() {
+  render: function() {
     var x = snake.headPosition[0];
     var y = snake.headPosition[1];
     var idName = "#" + x + "-" + y;
     $('#grid td').removeClass('snakeHead');
     $(idName).addClass('snakeHead');
   },
-  bodyPositions : [[20,20]],
-  move : function() {
-    console.log("moving")
+  bodyPositions: [[20,20]],
+  move: function() {
     switch (snake.direction) {
       case 'right':
         snake.headPosition[1] += 1;
@@ -43,28 +45,47 @@ var snake = {
         snake.headPosition[0] += 1;
         break;
     }
-    snake.render();
+  },
+  isOutOfBounds: function() {
+    var outOfBounds = false;
+    var x = snake.headPosition[1];
+    var y = snake.headPosition[0];
+    if (x < 0 || x > grid.size || y < 0 || y > grid.size) {
+      outOfBounds = true;
+    }
+    return outOfBounds;
   }
-}
+};
 
 function setUpMoveListeners() {
   $(document).keydown(function(event){
     snake.direction = event.key.substring(5, event.key.length).toLowerCase();
-  })
+  });
 }
 
 var game = {
-  start : function() {
-    setInterval(snake.move, 1000);
+  start: function() {
+    game.identifier = setInterval(game.turn, 1000);
+  },
+  turn: function() {
+    snake.move();
+    if ( snake.isOutOfBounds() ) {
+      game.over();
+    } else {
+      snake.render();
+    }
+  },
+  over: function() {
+    var gameOver = $("<div id=gameOver> Game Over </div>");
+    $('#grid').prepend(gameOver);
+    clearInterval(game.identifier);
   }
-}
+};
 
 $(document).ready(function(){
-  var gridSize = 20;
-  renderGrid(gridSize);
+  grid.render();
   setUpMoveListeners();
-  snake.initialPosition(gridSize);
-  snake.render();
+  snake.initialPosition();
   game.start();
 });
 
